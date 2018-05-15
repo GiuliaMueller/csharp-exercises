8c47078dfc7375a33c6f913b858c8bf290ac8a15
Giulia Mueller <giulia_mueller@yahoo.com>
2018-05-14 23:59:10 -0400

    if given radius, solve for area of a circle

---
+++ Area/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            double radius;
+            double area;
+
+            Console.WriteLine("What is the radius?");
+            input = Console.ReadLine();
+            radius = double.Parse(input);
+
+            if (radius < 0)
+            {
+                Console.WriteLine("radius must be greater than zero");
+                Console.ReadLine();
+
+            }
+            else
+            {
+                area = 3.14 * radius * radius;
+                Console.WriteLine("The area of a circle with radius " + radius + " is: " + area + "!");
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
