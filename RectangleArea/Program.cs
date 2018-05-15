9cd58114b414b5c320000a9ee94c701355fceedb
Giulia Mueller <giulia_mueller@yahoo.com>
2018-05-15 00:11:56 -0400

    given lenght and height, find the area of a rectangle

---
+++ RectangleArea/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length;
+            int height;
+            string input;
+            int result;
+
+            Console.WriteLine("What is the length of the rectangle?");
+            input = Console.ReadLine();
+            length = int.Parse(input);
+            Console.WriteLine("What is the height of the rectangle?");
+            input = Console.ReadLine();
+            height = int.Parse(input);
+            result = length * height;
+            Console.WriteLine("The area of the rectangle is " + result);
+            Console.ReadLine();
+        }
+    }
+}
